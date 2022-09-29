ffdd86cf1a62c5c822b397c2ffc3cd50464e0118
NCG <NCG@USERUSE-DHF9EG1>
2022-09-29 17:52:52 -0400

    Add project files.

---
+++ NUMBER ANALYZER/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Hello, Please enter a number (1-100)");
+string numberAnswer = Console.ReadLine();
+int number = Convert.ToInt32(Console.ReadLine);
+
+bool result = true;
+if (numberAnswer >= 60 && (5 % 2))
+{
+    Console.WriteLine($"The number is Odd and Less than 60");
+}
+
+bool result = true;
+if (numberAnswer <=2 && >= 24))
+{
+
+    Console.WriteLine($"The number is Even and Less than 25");
+}
+
+bool result = true;
+if (numberAnswer <=26 && >= 60))
+{
+    Console.WriteLine($"The number is even and between 26 and 60");
+
+}
+
+bool result = true;
+if (numberAnswer <60 && (4 % 2))
+{
+    Console.WriteLine($"The number is even and greater than 60");
+}
+
+bool result = true;
+if (numberAnswer <60 && (5 % 2))
+{
+    Console.WriteLine($"The number is Odd and Greater than 60");
+}
+
